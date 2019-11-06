cd11307cec5f29b4b3a11c699a37d3b677ccae0a
Edwin Ngwa <demoses@yahoo.com>
2019-11-06 02:34:23 -0500

    Model the data as JSON and functions refactoring

--- controller/patController.js
+++ controller/patController.js
@@ -3,77 +3,46 @@
  *
  */
 
-const patProjectService = require('../services/patProject.service.js');
-const path = require('path');
-const { check, validationResult } = require('express-validator');
-const _ = require('underscore');
+const patProjectService = require('../services/patProject.service.js')
+const path = require('path')
+const { check, validationResult } = require('express-validator')
+const _ = require('underscore')
 
-//******************************************************* 
+// *******************************************************
 
 module.exports = function (app) {
+
   /* Index page */
-  app.get('/', function (req, res,next) {
-
-    patProjectService.projectUser(req, res, next, req.body.giturl);
-
-    // TODO Enable user to delete the project folder and start over 
-
-  });
+  app.get('/', function (req, res, next) {
+    patProjectService.projectUser(req, res, next, req.body.giturl)
+  })
 
   /* Project page */
   app.post('/project', [
     // validate entry against a string or modify to use an array or regex
-    check('giturl').contains("sts")
-    // TODO should use URL instead refer to https://express-validator.github.io/docs/ 
-    // check('giturl').isURL('https://gitlab.com/xOPERATIONS')
+    check('giturl').contains('sts')
+  // TODO refer to https://express-validator.github.io/docs/
   ], function (req, res, next) {
-
-    // Finds the validation errors in this request and pass aen error message to the user
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      next(new Error("Invalid URL: Not a Recognized PAT project repository"));
+      next(new Error('Invalid URL: Not a Recognized PAT project repository'))
+    } else {
+      patProjectService.cloneProjectdir(req, res, next, req.body.giturl)
     }
-
-    else {
-
-      patProjectService.cloneProjectdir(req, res, next, req.body.giturl);
-
-    }
-    // TODO : removed session project dirafter session expiry  
-  });
+  })
 
   /* Project page if project already in local repos*/
   app.get('/projectfiles', function (req, res, next) {
-
-   // console.log(req.query.projectUrl);
-  //  res.send(req.query.projectUrl)
-    patProjectService.projectfiles(req, res, next);
-
-  });
+    patProjectService.projectfiles(req, res, next)
+  })
 
   /* Summary Timeline page */
   app.get('/summary', function (req, res, next) {
-   // console.log(req.query.projectUrl);
-   // res.send(req.query.projectUrl)
     patProjectService.projectTimeline(req, res, next)
-   
-
-  });
+  })
 
   /* Delete project files */
   app.post('/removeProject', function (req, res, next) {
-    // console.log(req.query.projectUrl);
-    // res.send(req.query.projectUrl)
-     patProjectService.deleteProjectFiles(req, res, next);
-    
- 
-   });
-
-
-  // res.send("POST")
-
-
-  //******************************************************* 
-
+    patProjectService.deleteProjectFiles(req, res, next)
+  })
 }; // end module.
-
