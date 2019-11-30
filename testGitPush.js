a88707bfc570be9ba595ed216788fec7728c0259
wegoerling <wegoerling@smcm.edu>
2019-11-29 20:56:04 -0500

    Adding new tests

---
+++ testGitPush.js
@@ -0,0 +1,152 @@
+/**
+ * Service to establish a link with a GitLab repository URL, 
+ * clone a PAT project, retrieve, process and map the data into objects
+ *  The app uses a document database to manipluate and render the data
+ *  based on a user request
+ *  
+ * */
+const assert = require('assert');
+const shell = require('shelljs');
+const path = require('path');
+const fs = require('fs');
+const dirTree = require('directory-tree');
+const serviceUtils = require('./services/serviceUtils');
+const _ = require('underscore');
+const projectModel = require('./model/projectModel.js');
+const timelineModel = require('./model/timelineModel.js');
+const procedureFolderName = 'procedures';
+const tasksFolderName = 'tasks';
+const procedureImage = 'images/';
+const patProjectdir = '../public/projects/';
+
+const reqt = {sessionID: 'fakeID-6b15-4e8f-9f73-1b891b043a31', body:{giturl:'https://gitlab.com/maestro-web-team/sts-134.git'}};
+const rest = {render: function(html, object){
+    console.log('res.render(' + html + ', ' + object + ')');
+    }
+};
+const nextt = function(err){console.log('error')};
+const patProceduredir = 'C:/public/projects/fakeID-6b15-4e8f-9f73-1b891b043a31/sts-134/procedures';
+const patProcedurefile = '/EVA1.yml';
+/**
+ * Push a Maestro project and create object models 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+*/
+const cloneProjectdir = function (req, res, next) {
+    var userSessionDir = patProjectdir + req.sessionID;
+    var giturl = req.body.giturl;
+    shell.mkdir('-p', patProjectdir, userSessionDir);
+    const simpleGit = require('simple-git')(userSessionDir);
+   
+    simpleGit.clone(giturl, function (err, data) {
+      if (err) {
+        shell.rm('-Rf', userSessionDir);
+        next(new Error(err)); // Pass errors to Express.
+      } else {
+  
+        var myMaestroProjectDoc = {
+          'projectName': path.basename(giturl),
+          'projectURL': giturl,
+          'userId': req.sessionID,
+          'procedureMeta': [],
+          'tasksMeta': [],
+          'images': []
+        };
+  
+        var procedureTimeline = {
+          'projectName': path.basename(giturl),
+          'projectURL': giturl,
+          'userId': req.sessionID,
+          'timeline': []
+        };
+  
+        var EvaProcedure = {
+          'userId': req.sessionID,
+          'columnHeaderText': [],
+          'procedureDetails': []
+        };
+        var EvaTasks = {
+          'userId': req.sessionID,
+          'tasksDetails': []
+        };
+       
+        // retrieve object from the procedure folder
+        var procedureMetaDoc = serviceUtils.patProjectData(userSessionDir, giturl, procedureFolderName, next, req);
+  
+        for (var x in procedureMetaDoc) {
+          let filename = procedureMetaDoc[x].name;
+          let procedureDoc = serviceUtils.JSONData(userSessionDir, giturl, procedureFolderName, filename, next, req);
+  
+          myMaestroProjectDoc.procedureMeta.push({
+            filename: procedureMetaDoc[x].name,
+            path: procedureMetaDoc[x].path,
+            procedure_name: procedureDoc.procedure_name
+          });
+  
+          EvaProcedure.columnHeaderText = serviceUtils.getColumnHeaderTextByActor(procedureDoc.columns);
+          EvaProcedure.procedureDetails = procedureDoc;
+        }
+  
+        // retrieve object from the task folder
+        var tasksMetaDoc = serviceUtils.patProjectData(userSessionDir, giturl, tasksFolderName, next, req);
+  
+        for (x in tasksMetaDoc) {
+          var taskfilename = tasksMetaDoc[x].name;
+          var taskDoc = serviceUtils.JSONData(userSessionDir, giturl, tasksFolderName, taskfilename, next, req);
+  
+          myMaestroProjectDoc.tasksMeta.push({
+            filename: taskfilename,
+            title: taskDoc.title,
+            path: tasksMetaDoc[x].path
+          });
+  
+          var tasks = taskDoc.roles;
+          for (var t in tasks) {
+            var duration = serviceUtils.timeConvert(tasks[t].duration.hours, tasks[t].duration.minutes);
+  
+            var obj = _.object(['file', 'title', 'crew', 'hours', 'minutes', 'cellHeight'], [taskfilename, taskDoc.title,
+              tasks[t].name, duration.hours, duration.minutes, duration.cellHeight
+            ]);
+            EvaTasks.tasksDetails.push(obj);
+          }
+  
+        }
+        // retrieve object from the image folder
+        var imagedoc = serviceUtils.patProjectData(userSessionDir, giturl, procedureImage, next, req);
+        for (x in imagedoc) {
+          myMaestroProjectDoc.images.push({
+            name: imagedoc[x].name,
+            path: imagedoc[x].path
+          });
+        }
+  
+        timelineModel.createObjects(EvaProcedure, EvaTasks, req, next);
+        
+        projectModel.createProject(userSessionDir, myMaestroProjectDoc, req, res, next);
+      }
+  
+    });
+}; // end module
+const pushProjectdir = function (req, res, next) {
+    var userSessionDir = patProjectdir + req.sessionID;
+    var giturl = req.body.giturl;
+    shell.mkdir('-p', patProjectdir, userSessionDir);
+    const simpleGit = require('simple-git')(userSessionDir);
+
+    simpleGit.commit('Automated Commit from Maestro-Web', patProceduredir+patProcedurefile);
+    return simpleGit.log();
+}
+
+describe('Testing using simpleGit to push files',function(){
+    //preamble start
+    var log = 0;
+    cloneProjectdir(reqt, rest, nextt);
+    log = pushProjectdir(reqt, rest, nextt);
+    //projectModel.deleteProjectFiles(reqt,rest,nextt);
+    //console.log(log);
+    it('should push successfully',function(){
+        //test start
+        assert.notEqual(log,0, 'Oops');
+    });
+});
---
+++ testServiceUtil.js
@@ -0,0 +1,108 @@
+const shell = require('shelljs');
+const path = require('path');
+const fs = require('fs');
+const dirTree = require('directory-tree');
+const _ = require('underscore');
+YAML = require('yamljs');
+
+const mockFolder = {
+    procedures:{name:'procedures', 'EVA1.yml':{name:'EVA1.yml', path:'sts-134/procedures/EVA1.yml'}}, 
+    tasks:{name:'tasks', 'egress.yml':{name:'egress.yml', path:'sts-134/tasks/egress.yml'}}
+};
+const ymlProcedure = {};
+const ymlTask = {};
+
+
+const patProjectData = function getProjectFolderData(userSessionDir, giturl, folderName, next, req) {
+    var projectFolderPath = getTree(userSessionDir + '/' + path.basename(giturl) + '/' + folderName);
+    if (_.isEmpty(projectFolderPath)) {
+      shell.rm('-Rf', userSessionDir);
+      next(new Error('This project ' + folderName + ' folder is empty or does not exist!'));
+    }
+    return projectFolderMeta = Object.byString(projectFolderPath, 'children');
+  
+  };
+
+const JSONData = function YAMLtoJSON(userSessionDir, giturl, folderName, filename, next, req) {
+  var projectFolderPath = userSessionDir + '/' + path.basename(giturl) + '/' + folderName;
+  var yamlFile = projectFolderPath + "/" + filename;
+  try {
+      const doc = YAML.load(yamlFile);
+      return doc;
+    } catch (e) {
+      console.log(e);
+  }
+};
+const YAMLData = function JSONtoYAML(jsonStr, userSessionDir, giturl, folderName, filename, next, req) {
+    var projectFolderPath = userSessionDir + '/' + path.basename(giturl) + '/' + folderName;
+    var yamlFile = projectFolderPath + "/" + filename;
+    try {
+      //var obj = JSON.parse(jsonStr);
+      console.log(projectFolderPath);
+      var yamlStr = YAML.stringify(jsonStr, 4); //> projectFolderPath/test.yml; 
+      var stream = fs.createWriteStream(projectFolderPath + '/test.yml');
+      stream.write(yamlStr);
+      return yamlStr;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  function getTree(dir) {
+    // get only yaml files
+    data = dirTree(dir, {
+      normalizePath: true,
+      extensions: /\.(yml|png)$/
+    }, (item, PATH, stats) => {});
+    return data;
+  }
+  Object.byString = function (o, s) {
+    s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+    s = s.replace(/^\./, ''); // strip a leading dot
+    var a = s.split('.');
+    for (var i = 0, n = a.length; i < n; ++i) {
+      var k = a[i];
+      if (k in o) {
+        o = o[k];
+      } else {
+        return;
+      }
+    }
+    return o;
+  };
+  function getColumnHeaderTextByActor(columns) {
+    var columnHeaderText = [];
+    for (var x in columns) {
+      if ((columns[x].actors) != "*") {
+        columnHeaderText.push(columns[x].display);
+      }
+  
+    }
+    return (columnHeaderText);
+  }
+  function timeConvert(taskHours, taskMin) {
+    let hours;
+    let mins;
+    let cellHeight;
+    if ((taskMin > 59)) {
+      hours = taskMin / 60;
+      mins = "00";
+      cellHeight = taskMin * 2;
+    } else if (_.isEmpty(taskHours)) {
+      hours = "00";
+      mins = taskMin;
+      cellHeight = taskMin * 2;
+    } else if (_.isEmpty(taskMin)) {
+      hours = "0" + taskHours;
+      mins = "00";
+      cellHeight = (taskHours * 60) * 2;
+    } else {
+      hours = "0" + taskHours;
+      mins = taskMin;
+      cellHeight = taskMin + (taskHours * 60) * 2;
+    }
+    return ({
+      hours: hours,
+      minutes: mins,
+      cellHeight: cellHeight
+    });
+  }
